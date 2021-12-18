df46609a79f64f7edea920e178824f7c5fccaa5d
Asus <17520217@gm.uit.edu,vn>
2021-12-18 21:58:09 +0700

    Extract IA to another package (less complex)

--- src/classes_and_interfaces/IA2.java
+++ src/classes_and_interfaces/IA2.java
@@ -1,7 +1,5 @@
 package p9.classes_and_interfaces;
 
 public interface IA2 {
-    void m6();
-    void m7();
 //    void m9();
 }
---
+++ src/folder/folder11/IA.java
@@ -0,0 +1,6 @@
+package p9.folder.folder11;
+
+public interface IA {
+    void m6();
+    void m7();
+}
