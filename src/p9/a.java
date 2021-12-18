44b8a01d5ad1ad0e8509892e421376d456f49d0e
Hoanguitk12 <luna02091999@gmail.com>
2021-12-18 18:13:03 +0700

    add some method class A p9

--- src/p9/a.java
+++ src/p9/a.java
@@ -1,5 +1,11 @@
 package p9;
 
 public class A {
- 
+  public void tesst() {
+    
+    System.out.println("tesstttt class A"); 
+  }
+  public void test2() {
+    System.out.println("tesstttt 2 class A"); 
+  }
 }
