538530057d30f67c2d29b7fa74973dd294307157
Asus <17520217@gm.uit.edu,vn>
2021-12-18 18:53:53 +0700

    Move 11 back to class A

--- src/a.java
+++ src/a.java
@@ -1,6 +1,9 @@
 package p9;
 
 class A extends C implements IA2 {
+    public void m11() {
+        System.out.println("Do something");
+    }
 
     public void m9() {
         System.out.println("Method 9");
@@ -37,7 +40,5 @@ class C {
 }
 
 class D {
-    public void m12() {
-        System.out.println("Do something");
-    }
+
 }
