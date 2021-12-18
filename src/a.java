9b466ce191c4bab1c098ee2eb295c8d005818f98
Asus <17520217@gm.uit.edu,vn>
2021-12-18 10:00:25 +0700

    Extracted interface

--- src/a.java
+++ src/a.java
@@ -1,12 +1,22 @@
 package p9;
 
-class A {
-  public void m6() {
-    int value = 5;
-    System.out.println("m" + value);
-  }
+class A implements IA {
+    public void m6() {
+        int value = 5;
+        System.out.println("m" + value);
+    }
 
-  public void m7() {
-    System.out.println("Do something");
-  }
+    public void m7() {
+        System.out.println("Do something");
+    }
+
+    public void m8() {
+        System.out.println("Hello");
+    }
+}
+
+interface IA {
+    void m6();
+    void m7();
+    void m8();
 }
