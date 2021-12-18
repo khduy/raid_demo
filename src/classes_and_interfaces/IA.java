2df4573982a2482c89504d6f2c09a02d90811f7b
Asus <17520217@gm.uit.edu,vn>
2021-12-18 21:49:07 +0700

    Move m6, m7 methods to IA2

--- src/classes_and_interfaces/IA.java
+++ src/classes_and_interfaces/IA.java
@@ -1,6 +1,5 @@
 package p9.classes_and_interfaces;
 
 public interface IA {
-    void m6();
-    void m7();
+
 }
--- src/classes_and_interfaces/IA2.java
+++ src/classes_and_interfaces/IA2.java
@@ -1,5 +1,7 @@
 package p9.classes_and_interfaces;
 
-public interface IA2 extends IA3 {
-    void m9();
+public interface IA2 {
+    void m6();
+    void m7();
+//    void m9();
 }
