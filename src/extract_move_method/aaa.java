1942ccf22ad75f71cf10cf6ebebafa88830db327
khduy <duyhk1223@gmail.comm>
2021-12-18 17:21:07 +0700

    init demo

--- src/extract_move_method/aaa.java
+++ src/extract_move_method/aaa.java
@@ -1,11 +1,7 @@
 public class Aaa {
-    public boolean isEqualZero(int i) {
-        return i == 0;
-    }
-        
     public void foo() {
         int i = 0;
-        if (isEqualZero(i)) {
+        if (i==0) {
             System.out.println("i is 0");
         } else {
             System.out.println("i is not 0");
