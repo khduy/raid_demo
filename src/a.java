88644786a52798c4df6102ec4243fb32f6aaa1ee
Asus <17520217@gm.uit.edu,vn>
2021-12-18 09:36:48 +0700

    Changed m6, m7 name

--- src/a.java
+++ src/a.java
@@ -1,12 +1,12 @@
 package p9;
 
 class A {
-  public void m7() {
+  public void m6() {
     int value = 5;
     System.out.println("m" + value);
   }
 
-  public void m6() {
+  public void m7() {
     System.out.println("Do something");
   }
 }
