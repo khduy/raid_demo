8af8502d65d5e1a763ead6190bed9b1501a6d719
Asus <17520217@gm.uit.edu,vn>
2021-12-18 21:32:07 +0700

    Merged with master

--- src/A.java
+++ src/A.java
@@ -1,6 +1,6 @@
 package p9;
 
-class A implements IA {
+public class A implements IA {
     public void m6() {
         System.out.println("Method m6");
     }
@@ -26,31 +26,19 @@ class A implements IA {
 //    void m10();
 //}
 
-interface IA2 extends IA3 {
-    void m9();
-}
-
-interface IA3 {
-    void m10();
-}
-
-class B extends C {
-
-}
-
-class C {
-    public void m6() {
-        int value = 5;
-        System.out.println("m" + value);
-    }
-}
-
-class D {
-    public void test() {
-        System.out.println("Test method");
-    }
-
-    public void m8() {
-        System.out.println("Do something");
-    }
-}
+//class C {
+//    public void m6() {
+//        int value = 5;
+//        System.out.println("m" + value);
+//    }
+//}
+//
+//class D {
+//    public void test() {
+//        System.out.println("Test method");
+//    }
+//
+//    public void m8() {
+//        System.out.println("Do something");
+//    }
+//}
