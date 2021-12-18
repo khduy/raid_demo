d3e373ae03cf14c796b1b255f4082a53032a8fe0
khduy <duyhk1223@gmail.comm>
2021-12-18 17:52:45 +0700

    extract: extract and move method isEqualZero from foo method to class Y

--- src/extract_move_method/X.java
+++ src/extract_move_method/X.java
@@ -1,9 +1,11 @@
 package extract_move_method;
 
 public class X {
+   Y y = new Y();
+
     public void foo() {
         int value = 0;
-        if (value == 0) {
+        if (y.isEqualZero(value)) {
             System.out.print("value is 0");
         } else {
             System.out.print("value is not 0");
