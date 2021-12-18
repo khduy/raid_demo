7d0e49f125c021e884d4785ee59fe7e74723618c
Asus <17520217@gm.uit.edu,vn>
2021-12-18 11:05:24 +0700

    Extract IA2 from class ABC

--- src/a.java
+++ src/a.java
@@ -1,6 +1,6 @@
 package p9;
 
-class ABC {
+class ABC implements IA2 {
     public void m6() {
         int value = 5;
         System.out.println("m" + value);
@@ -19,10 +19,14 @@ class ABC {
     }
 }
 
-interface IA /*extends IA2*/ {
+interface IA extends IA2 {
     void m6();
 
     void m7();
+}
+
+interface IA2 extends IA3 {
+    void m9();
 }
 
 interface IA3 {
