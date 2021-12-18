00572d470aefa33cd3fb2c853ffd801fa138debf
Asus <17520217@gm.uit.edu,vn>
2021-12-18 16:57:28 +0700

    Extract m8 and m8Sec methods from Folder8 class

--- src/folder2/IFolder2.java
+++ src/folder2/IFolder2.java
@@ -1,4 +1,6 @@
 package p9.folder2;
 
 public interface IFolder2 {
+    void m8();
+    void m8Sec();
 }
--- src/folder2/folder5/folder6/folder8/Folder8.java
+++ src/folder2/folder5/folder6/folder8/Folder8.java
@@ -1,11 +1,13 @@
 package p9.folder2.folder5.folder6.folder8;
 
-class Folder8 {
-    void m8() {
+import p9.folder2.IFolder2;
+
+class Folder8 implements IFolder2 {
+    public void m8() {
         System.out.println("Method 8");
     }
 
-    void m8Sec() {
+    public void m8Sec() {
         System.out.println("Method m8 second");
     }
 }
