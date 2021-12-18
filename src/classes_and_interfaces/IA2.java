7e82cf726e4e9f3e7bffd1c45cd9b86e4a8418d1
Asus <17520217@gm.uit.edu,vn>
2021-12-18 21:42:10 +0700

    Add new package

--- src/classes_and_interfaces/IA2.java
+++ src/classes_and_interfaces/IA2.java
@@ -1,4 +1,4 @@
-package p9;
+package p9.classes_and_interfaces;
 
 public interface IA2 extends IA3 {
     void m9();
--- src/classes_and_interfaces/IA3.java
+++ src/classes_and_interfaces/IA3.java
@@ -1,4 +1,4 @@
-package p9;
+package p9.classes_and_interfaces;
 
 public interface IA3 {
     void m10();
