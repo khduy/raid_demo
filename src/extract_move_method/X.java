6649d37d374693a115b85268e66f491b7de2432e
khduy <duyhk1223@gmail.comm>
2021-12-18 18:59:49 +0700

    Init demo extract and move method

---
+++ src/extract_move_method/X.java
@@ -0,0 +1,12 @@
+package extract_move_method;
+
+public class X {
+    public void foo() {
+        int value = 0;
+        if (value == 0) {
+            System.out.print("value is 0");
+        } else {
+            System.out.print("value is not 0");
+        }
+    }
+}
