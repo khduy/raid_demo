fee0426eaa88c8a7e1d2589b46fb2353733d6bb5
khduy <duyhk1223@gmail.comm>
2021-12-18 17:02:52 +0700

    extract: extract method isEqualZero from foo method

--- src/extract_move_method/aaa.java
+++ src/extract_move_method/aaa.java
@@ -1,7 +1,11 @@
 public class Aaa {
+    public boolean isEqualZero(int i) {
+        return i == 0;
+    }
+        
     public void foo() {
         int i = 0;
-        if (i == 0) {
+        if (isEqualZero(i)) {
             System.out.println("i is 0");
         } else {
             System.out.println("i is not 0");
