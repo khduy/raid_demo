986ffd99ae05179f717e4ef4cd11a24c8934e01a
Asus <17520217@gm.uit.edu,vn>
2021-12-19 00:20:57 +0700

    Move m6 back to A

--- src/classes_and_interfaces/A.java
+++ src/classes_and_interfaces/A.java
@@ -1,6 +1,9 @@
 package p9.classes_and_interfaces;
 
-public class A extends B {
+public class A {
+    public void m6() {
+        System.out.println("Method m6");
+    }
 
     public void m7() {
         System.out.println("Method m7");
