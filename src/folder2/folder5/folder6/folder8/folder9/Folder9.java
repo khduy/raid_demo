8d0e9dcde6ac6bfc220136e865c9b79c57592329
Asus <17520217@gm.uit.edu,vn>
2021-12-18 18:04:47 +0700

    Extract super class from Folder9 class

--- src/folder2/folder5/folder6/folder8/folder9/Folder9.java
+++ src/folder2/folder5/folder6/folder8/folder9/Folder9.java
@@ -1,11 +1,7 @@
 package p9.folder2.folder5.folder6.folder8.folder9;
 
-class Folder9 {
+class Folder9 extends  Folder9Sec {
     public void m9() {
         System.out.println("Method 9");
-    }
-
-    public void m9Sec() {
-        System.out.println("Method m9 second");
     }
 }
