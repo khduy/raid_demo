12aa69e129d11fe2befa0482146e712f30a4d09b
khduy <duyhk1223@gmail.comm>
2021-12-18 19:22:36 +0700

    refactor: pull up method getHP()

--- src/pull_up_push_down_method/Tank.java
+++ src/pull_up_push_down_method/Tank.java
@@ -1,9 +1,7 @@
 package pull_up_push_down_method;
 
 public class Tank extends Unit {
-    public void getHP() {
-        System.out.println("Getting HP");
-    }
+    
 
     public void fillFuel() {
         System.out.println("Filling fuel");
--- src/pull_up_push_down_method/Unit.java
+++ src/pull_up_push_down_method/Unit.java
@@ -2,5 +2,8 @@ package pull_up_push_down_method;
 
 public class Unit {
     int speed = 0;
+    public void getHP() {
+        System.out.println("Getting HP");
+    }
     
 }
