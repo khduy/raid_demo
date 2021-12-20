429ad27d7e73925d3b020ff4801e87fca03e7e68
khduy <duyhk1223@gmail.comm>
2021-12-20 14:21:51 +0700

    refactor: extract move method to dif package

--- src/extract_move_method/X.java
+++ src/extract_move_method/X.java
@@ -1,18 +1,21 @@
 package extract_move_method;
 
+import another_extract_move.Z;
+
 public class X {
-   Y y = new Y();
+    Y y = new Y();
+    Z z = new Z();
 
     public void foo() {
         int value = 0;
-        if (y.isEqualZero(value)) {
+        if (z.isEqualZero(value)) {
             System.out.print("value is 0");
         } else {
             System.out.print("value is not 0");
         }
     }
 
-    public void doStep1(){
+    public void doStep1() {
         System.out.print("Do step 1.1");
         System.out.print("Do step 1.2");
         System.out.print("Do step 1.3");
--- src/extract_move_method/Y.java
+++ src/extract_move_method/Y.java
@@ -1,7 +1,5 @@
 package extract_move_method;
 
 public class Y {
-    public boolean isEqualZero(int x) {
-        return x == 0;
-    }
+    
 }
