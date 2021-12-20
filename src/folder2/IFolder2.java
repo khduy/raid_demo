6794e542712bc29db79da518d74cd188c98b888b
Hoanguitk12 <luna02091999@gmail.com>
2021-12-20 14:48:29 +0700

    move class folder8 from folder8.java to ifolder9.java then rename

--- src/folder2/IFolder2.java
+++ src/folder2/IFolder2.java
@@ -3,3 +3,4 @@ package p9.folder2;
 public interface IFolder2 extends IFolder2Sec {
     void m8();
 }
+
--- src/folder2/folder5/folder6/folder8/folder9/IFolder9.java
+++ src/folder2/folder5/folder6/folder8/folder9/IFolder9.java
@@ -3,3 +3,17 @@ package p9.folder2.folder5.folder6.folder8.folder9;
 public interface IFolder9 {
     void m5();
 }
+
+class Folder8test implements IFolder2 {
+    public void m8() {
+        System.out.println("Method 8");
+    }
+
+    public void m8Sec() {
+        System.out.println("Method m8 second");
+    }
+
+    public void m8Thi( ) {
+        System.out.println("Method m8 third");
+    }
+}
