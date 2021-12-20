75fa9965a9a8e15d3c0cadf751858b32f6eae39a
Asus <17520217@gm.uit.edu,vn>
2021-12-20 15:00:38 +0700

    Remove class B in A.java

--- src/classes_and_interfaces/A.java
+++ src/classes_and_interfaces/A.java
@@ -1,6 +1,6 @@
 package p9.classes_and_interfaces;
 
-public class A extends B implements IA1 {
+public class A implements IA1 {
 
     public void m8() {
         System.out.println("Method m7");
@@ -13,10 +13,6 @@ public class A extends B implements IA1 {
     public void m11() {
         System.out.println("Method 10");
     }
-}
-
-class B {
-
 }
 
 interface IA1 {
