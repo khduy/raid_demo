349dcdb2d7b201024a861726e505b8423231ae4c
Asus <17520217@gm.uit.edu,vn>
2021-12-20 11:50:00 +0700

    Remove implement interface IA from class A in class_and_interfaces

--- src/classes_and_interfaces/A.java
+++ src/classes_and_interfaces/A.java
@@ -1,8 +1,6 @@
 package p9.classes_and_interfaces;
 
-import p9.folder.folder11.IA;
-
-public class A implements IA {
+public class A {
 
     public void m8() {
         System.out.println("Method m7");
