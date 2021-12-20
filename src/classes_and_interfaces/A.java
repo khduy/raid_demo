1e74fa64191d25b756ecfa9d8e2b83b48ec6a585
Asus <17520217@gm.uit.edu,vn>
2021-12-20 11:52:17 +0700

    Extract interface IA from class A, IA and class A are in different folders (folder contain the interface is depth inside another folders)

--- src/classes_and_interfaces/A.java
+++ src/classes_and_interfaces/A.java
@@ -1,6 +1,8 @@
 package p9.classes_and_interfaces;
 
-public class A {
+import p9.folder2.folder5.folder6.folder8.folder9.IA;
+
+public class A implements IA {
 
     public void m8() {
         System.out.println("Method m7");
